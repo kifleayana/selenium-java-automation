7591d8d84a97daa031d09508ea0052086338241a
kifleayana <145468024+kifleayana@users.noreply.github.com>
2023-10-02 19:18:13 +0530

    Update ActionEngine.java

--- src/main/java/com/java/selenium/actions/ActionEngine.java
+++ src/main/java/com/java/selenium/actions/ActionEngine.java
@@ -80,7 +80,7 @@ public class ActionEngine extends TestBase {
 
 	public void waitForElementToBeClickable(By locator, int waitTime) throws Throwable {
 
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(waitTime));
+		WebDriverWait wait = new WebDriverWait(driver, 5);
 		wait.until(ExpectedConditions.elementToBeClickable(locator));
 
 	}
@@ -98,19 +98,19 @@ public class ActionEngine extends TestBase {
 	}
 
 	public void acceptAlert() throws Throwable {
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		WebDriverWait wait = new WebDriverWait(driver, 5);
 		Alert alert = wait.until(ExpectedConditions.alertIsPresent());
 		alert.accept();
 	}
 
 	public void dismissAlert() throws Throwable {
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		WebDriverWait wait = new WebDriverWait(driver, 5);
 		Alert alert = wait.until(ExpectedConditions.alertIsPresent());
 		alert.dismiss();
 	}
 
 	public void sendDataTosAlert(String str) throws Throwable {
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		WebDriverWait wait = new WebDriverWait(driver, 5);
 		Alert alert = wait.until(ExpectedConditions.alertIsPresent());
 		alert.sendKeys(str);
 	}
